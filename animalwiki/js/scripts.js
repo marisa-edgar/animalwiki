6f82d2924890fde2f907245f5903a041af15eb56
marisa-edgar <marisaedgar1212@gmail.com>
2021-10-21 10:45:05 -0700

    hide divs
    Co-authored-by: Matt Casperson <mc.casperson@gmail.com>

--- animalwiki/js/scripts.js
+++ animalwiki/js/scripts.js
@@ -3,12 +3,18 @@ $(document).ready(function() {
     event.preventDefault();
     const animal = parseInt($("#animal").val());
 
-    if (animal = 1) {
+    if (animal === 1) {
       $(".sloth").show();
-    } else if (animal = 2) {
+      $(".hedgehog").hide();
+      $(".squirrel").hide();
+    } else if (animal === 2) {
       $(".hedgehog").show();
-    } else if (animal = 3) {
+      $(".squirrel").hide(); 
+      $(".sloth").hide();
+    } else if (animal === 3) {
       $(".squirrel").show();
+      $(".hedgehog").hide();
+      $(".sloth").hide();
     } else {
       alert("please pick an animal");
     }
